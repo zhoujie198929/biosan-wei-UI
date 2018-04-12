9ed189ba501a9a03565e1ad0239375a58ebaca13
zhoujie <zhoujie@biosan-screening.com>
2018-04-12 16:15:54 +0800

    修改noticebar的js文件和模板文件 、less文件

--- components/noticeBar/index.js
+++ components/noticeBar/index.js
@@ -1,8 +1,27 @@
+function resolveClickLink() {
+    if (this.handleActionLinkClick) {
+        this.handleActionLinkClick();
+    } else {
+        console.warn('页面缺少 handleActionLinkClick 回调函数');
+    }
+}
+
 const bsNoticeBarController = {
-    $bs_dialog_init: function (options) {
+    $bs_notice_bar_init: function (options) {
+        let {
+            action:'',
+            content:'notice content',
+            mode:'closeable',
+            icon:'',
+            show:true
+        }=options;
         this.setData({
             bs_notice_bar: {
-                options
+                action,
+                content,
+                mode,
+                icon,
+                show
             }
         });
     },
@@ -10,13 +29,17 @@ const bsNoticeBarController = {
         let item=e.currentTarget.dataset.content;
         let mode=item.mode;
         if(mode=='link'){
-
+            resolveClickLink.call(this);
         }else if(mode==='closeable'){
-
+            this.setData({
+                'bs_notice_bar.show':false
+            })
         }
     },
     $bs_notice_bar_show:function(option={}){
-
+        this.setData({
+            'bs_notice_bar.show':true
+        })
     },
 
 };
